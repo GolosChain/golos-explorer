388006fd1a8e58adce734b36ff9881fb8aaaa36a
Sing Painter <mail@erlan.xyz>
2018-03-15 17:46:11 +0300

    + add account transfers info + add loading animate

--- app.js
+++ app.js
@@ -28,6 +28,12 @@ let $recentBlocksTable = document.getElementById('recent-blocks');
 let $recentBlocksTableTbody = $recentBlocksTable.getElementsByTagName('tbody')[0];
 let $aboutBlockTable = document.getElementById('about-block');
 let $aboutBlockTableTbody = $aboutBlockTable.getElementsByTagName('tbody')[0];
+let $resetBlockBtn = document.getElementById('reset-block');
+let $resetAccountBtn = document.getElementById('reset-account');
+let $aboutAccountTable = document.getElementById('about-account');
+let $aboutAccountTableTbody = $aboutAccountTable.getElementsByTagName('tbody')[0];
+let $loader = document.getElementsByClassName('lding')[0];
+let $recentBlocksInfo = document.getElementById('recent-blocks-info');
 
 let getLastBlockInterval = setInterval(function() {
 	
@@ -78,39 +84,108 @@ document.getElementById('search-block').addEventListener('submit', function(e) {
 	e.preventDefault();
 	$recentBlocksTable.style.display = 'none';
 	$aboutBlockTable.style.display = 'table';
+	$resetBlockBtn.style.display = 'block';
 	let blockNumberVal = this.querySelector('.form-control[name="block-number"]').value;
+	document.getElementById('search-account').querySelector('.form-control[name="account-username"]').value = '';
+	$aboutAccountTable.style.display = 'none';
+	$resetAccountBtn.style.display = 'none';
+	$recentBlocksInfo.style.display = 'none';
 	golos.api.getBlock(blockNumberVal, function(err, block) {
-		//console.log(err, 'getBlock: ', block);
-		let operations = {};
-		let operationsCount = 0;
-		block.transactions.forEach(function(transaction) {
-			transaction.operations.forEach(function(operation) {
-				if ( ! operations[operation[0]]) operations[operation[0]] = 0;
-				operations[operation[0]]++;
-				operationsCount++;
+		console.log(err, 'getBlock: ', block);
+		if ( ! err) {
+			let operations = {};
+			let operationsCount = 0;
+			block.transactions.forEach(function(transaction) {
+				transaction.operations.forEach(function(operation) {
+					if ( ! operations[operation[0]]) operations[operation[0]] = 0;
+					operations[operation[0]]++;
+					operationsCount++;
+				});
 			});
-		});
-		let operationsStr = '';
-		for (let key in operations) {
-			operationsStr += `<a class="btn btn-outline-secondary btn-sm">${key} <span class="badge badge-secondary">${operations[key]}</span></a> `; 
+			let operationsStr = '';
+			for (let key in operations) {
+				operationsStr += `<a class="btn btn-outline-secondary btn-sm">${key} <span class="badge badge-secondary">${operations[key]}</span></a> `; 
+			}
+			console.debug(blockNumberVal, block.timestamp, block.witness, block.transactions.length, operationsCount, operationsStr);
+
+			let $newRow = $aboutBlockTableTbody.insertRow(0);
+			$newRow.innerHTML = `<tr>
+									<td><a href="#">${blockNumberVal}</a></td>
+									<td>${block.timestamp}</td>
+									<td><a href="#">${block.witness}</a></td>
+									<td>${block.transactions.length}</td>
+									<td>${operationsCount}</td>
+								</tr>`;
+
+			$newRow = $aboutBlockTableTbody.insertRow(1);
+			$newRow.innerHTML = `<tr>
+									<td colspan="5"><span class="badge badge-secondary">Operations:</span> ${operationsStr}</td>
+								</tr>`;
 		}
-		console.debug(blockNumberVal, block.timestamp, block.witness, block.transactions.length, operationsCount, operationsStr);
-
-		let $newRow = $aboutBlockTableTbody.insertRow(0);
-		$newRow.innerHTML = `<tr>
-								<td><a href="#">${blockNumberVal}</a></td>
-								<td>${block.timestamp}</td>
-								<td><a href="#">${block.witness}</a></td>
-								<td>${block.transactions.length}</td>
-								<td>${operationsCount}</td>
-							</tr>`;
-
-		$newRow = $aboutBlockTableTbody.insertRow(1);
-		$newRow.innerHTML = `<tr>
-								<td colspan="5"><span class="badge badge-secondary">Operations:</span> ${operationsStr}</td>
-							</tr>`;
+		else swal({title: 'Error', type: 'error', text: err});
 	});
 	return false;
+});
+
+$resetBlockBtn.addEventListener('click', function() {
+	document.getElementById('search-block').querySelector('.form-control[name="block-number"]').value = '';
+	$resetBlockBtn.style.display = 'none';
+	$recentBlocksTable.style.display = 'table';
+	$aboutBlockTable.style.display = 'none';
+	document.getElementById('search-account').querySelector('.form-control[name="account-username"]').value = '';
+	$aboutAccountTable.style.display = 'none';
+	$resetAccountBtn.style.display = 'none';
+	$recentBlocksInfo.style.display = 'block';
+});
+
+document.getElementById('search-account').addEventListener('submit', function(e) {
+	e.preventDefault();
+	$loader.style.display = 'block';
+	$recentBlocksTable.style.display = 'none';
+	$aboutAccountTable.style.display = 'block';
+	$resetAccountBtn.style.display = 'block';
+	let usernameVal = this.querySelector('.form-control[name="account-username"]').value;
+	document.getElementById('search-block').querySelector('.form-control[name="block-number"]').value = '';
+	$resetBlockBtn.style.display = 'none';
+	$aboutBlockTable.style.display = 'none';
+	let transfersCount = 0;
+	$aboutAccountTableTbody.innerHTML = '';
+	$recentBlocksInfo.style.display = 'none';
+	golos.api.getAccountHistory(usernameVal, -1, 100, function(err, transactions) {
+		//console.log(err, 'getAccountHistory: ', transactions);
+		$loader.style.display = 'none';
+		if ( ! err) {
+			transactions.reverse();
+			transactions.forEach(function(transaction) {
+				if (transaction[1].op[0] == 'transfer') {
+					transfersCount++;
+					console.debug(transaction[1].timestamp, transaction[1].op[1]);
+					let $newRow = $aboutAccountTableTbody.insertRow(0);
+					$newRow.innerHTML = `<tr>
+									<td>${transaction[1].timestamp}</td>
+									<td>${transaction[1].op[1].from}</td>
+									<td>${transaction[1].op[1].to}</td>
+									<td>${transaction[1].op[1].amount}</td>
+									<td>${transaction[1].op[1].memo}</td>
+								</tr>`;
+				}
+			});
+			if (transfersCount == 0) swal({title: 'Error', type: 'error', text: 'This account did not make any translations!'});
+		}
+		else swal({title: 'Error', type: 'error', text: err});
+	});
+	return false;
+});
+
+$resetAccountBtn.addEventListener('click', function() {
+	document.getElementById('search-block').querySelector('.form-control[name="block-number"]').value = '';
+	$resetBlockBtn.style.display = 'none';
+	$recentBlocksTable.style.display = 'table';
+	$aboutBlockTable.style.display = 'none';
+	document.getElementById('search-account').querySelector('.form-control[name="account-username"]').value = '';
+	$aboutAccountTable.style.display = 'none';
+	$resetAccountBtn.style.display = 'none';
+	$recentBlocksInfo.style.display = 'block';
 });
 
 let getTransaction = function() {
