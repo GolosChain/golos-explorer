cc9d31bc0483a56f65270747a628ca7d564425e0
Sing Painter <mail@erlan.xyz>
2018-03-15 10:54:24 +0300

    + add html template

---
+++ app.js
@@ -0,0 +1,8 @@
+swal.setDefaults({
+	buttonsStyling: true,
+	confirmButtonText: '<span class="icon-checkmark"></span> Ok',
+	confirmButtonColor: '#5cb85c',
+	cancelButtonText: '<span class="icon-cross"></span> Cancel',
+	cancelButtonColor: '#d9534f',
+});
+
