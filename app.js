b618c26a534403239bd7d6ab5e032927927a0bd3
Sing Painter <mail@erlan.xyz>
2018-03-15 14:22:16 +0300

    + add get information about one block

--- app.js
+++ app.js
@@ -6,9 +6,7 @@ swal.setDefaults({
 	cancelButtonColor: '#d9534f',
 });
 
-golos.api.setWebSocket('wss://server-ws.golos.io');
-
-var getLastBlock = function(callback) {
+let getLastBlock = function(callback) {
 	
 	golos.api.getDynamicGlobalProperties(function(err, properties) {
 		//console.log(err, 'getDynamicGlobalProperties: ', properties);
@@ -22,13 +20,16 @@ var getLastBlock = function(callback) {
 	
 };
 
-var blocksIds = {};
+let blocksIds = {};
 
-var $headBlockNumber = document.getElementById('head-block-number');
-var $reverseBlocksCount = document.getElementById('revers-blocks-count');
-var $recentBlocksTable = document.getElementById('recent-blocks').getElementsByTagName('tbody')[0];
+let $headBlockNumber = document.getElementById('head-block-number');
+let $reverseBlocksCount = document.getElementById('revers-blocks-count');
+let $recentBlocksTable = document.getElementById('recent-blocks');
+let $recentBlocksTableTbody = $recentBlocksTable.getElementsByTagName('tbody')[0];
+let $aboutBlockTable = document.getElementById('about-block');
+let $aboutBlockTableTbody = $aboutBlockTable.getElementsByTagName('tbody')[0];
 
-var getLastBlockInterval = setInterval(function() {
+let getLastBlockInterval = setInterval(function() {
 	
 	getLastBlock(function(properties, block) {
 		//console.log(properties);
@@ -50,12 +51,12 @@ var getLastBlockInterval = setInterval(function() {
 				});
 			});
 			let operationsStr = '';
-			for (var key in operations) {
+			for (let key in operations) {
 				operationsStr += `<a class="btn btn-outline-secondary btn-sm">${key} <span class="badge badge-secondary">${operations[key]}</span></a> `; 
 			}
 			console.debug(properties.last_irreversible_block_num, block.timestamp, block.witness, block.transactions.length, operationsCount, operationsStr);
 			
-			let $newRow = $recentBlocksTable.insertRow(0);
+			let $newRow = $recentBlocksTableTbody.insertRow(0);
 			$newRow.innerHTML = `<tr>
 									<td><a href="#">${properties.last_irreversible_block_num}</a></td>
 									<td>${block.timestamp}</td>
@@ -64,7 +65,7 @@ var getLastBlockInterval = setInterval(function() {
 									<td>${operationsCount}</td>
 								</tr>`;
 			
-			$newRow = $recentBlocksTable.insertRow(1);
+			$newRow = $recentBlocksTableTbody.insertRow(1);
 			$newRow.innerHTML = `<tr>
 									<td colspan="5"><span class="badge badge-secondary">Operations:</span> ${operationsStr}</td>
 								</tr>`;
@@ -73,7 +74,46 @@ var getLastBlockInterval = setInterval(function() {
 
 }, 1000);
 
-var getTransaction = function() {
+document.getElementById('search-block').addEventListener('submit', function(e) {
+	e.preventDefault();
+	$recentBlocksTable.style.display = 'none';
+	$aboutBlockTable.style.display = 'table';
+	let blockNumberVal = this.querySelector('.form-control[name="block-number"]').value;
+	golos.api.getBlock(blockNumberVal, function(err, block) {
+		//console.log(err, 'getBlock: ', block);
+		let operations = {};
+		let operationsCount = 0;
+		block.transactions.forEach(function(transaction) {
+			transaction.operations.forEach(function(operation) {
+				if ( ! operations[operation[0]]) operations[operation[0]] = 0;
+				operations[operation[0]]++;
+				operationsCount++;
+			});
+		});
+		let operationsStr = '';
+		for (let key in operations) {
+			operationsStr += `<a class="btn btn-outline-secondary btn-sm">${key} <span class="badge badge-secondary">${operations[key]}</span></a> `; 
+		}
+		console.debug(blockNumberVal, block.timestamp, block.witness, block.transactions.length, operationsCount, operationsStr);
+
+		let $newRow = $aboutBlockTableTbody.insertRow(0);
+		$newRow.innerHTML = `<tr>
+								<td><a href="#">${blockNumberVal}</a></td>
+								<td>${block.timestamp}</td>
+								<td><a href="#">${block.witness}</a></td>
+								<td>${block.transactions.length}</td>
+								<td>${operationsCount}</td>
+							</tr>`;
+
+		$newRow = $aboutBlockTableTbody.insertRow(1);
+		$newRow.innerHTML = `<tr>
+								<td colspan="5"><span class="badge badge-secondary">Operations:</span> ${operationsStr}</td>
+							</tr>`;
+	});
+	return false;
+});
+
+let getTransaction = function() {
 	
 	let trx = {
 		ref_block_num: 49191,
