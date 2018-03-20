b76d122ea0019521f20a942fb5a9ea521bf3e96a
Sing Painter <mail@erlan.xyz>
2018-03-20 20:08:27 +0300

    * remove setInterval hell! replace on streamBlockNumber & streamBlock * return version golos-js lib from 0.6.1 to 0.5.30

--- app.js
+++ app.js
@@ -6,20 +6,6 @@ swal.setDefaults({
 	cancelButtonColor: '#d9534f',
 });
 
-let getLastBlock = function(callback) {
-	
-	golos.api.getDynamicGlobalProperties(function(err, properties) {
-
-		golos.api.getBlock(properties.last_irreversible_block_num, function(err, block) {
-			callback(properties, block);
-		});
-
-	});
-	
-};
-
-let blocksIds = {};
-
 let $headBlockNumber = document.getElementById('head-block-number');
 let $reverseBlocksCount = document.getElementById('revers-blocks-count');
 let $mainPage = document.getElementById('main-page');
@@ -57,9 +43,11 @@ let getChainProperties = function() {
 }
 getChainProperties();
 
-let getLastBlockInterval = setInterval(function() {
+let lastBlock;
+golos.api.streamBlockNumber(function(err, block) {
+	lastBlock = block;
 	
-	getLastBlock(function(properties, block) {
+	golos.api.getDynamicGlobalProperties(function(err, properties) {
 		for (let key in properties) {
 			let prop = $globalPropertiesTableTbody.querySelector('b[data-prop="' + key + '"]');
 			if (prop) prop.innerHTML = properties[key];
@@ -67,39 +55,40 @@ let getLastBlockInterval = setInterval(function() {
 		let reverseBlockCount = properties.head_block_number - properties.last_irreversible_block_num;
 		$headBlockNumber.innerHTML = properties.head_block_number;
 		$reverseBlocksCount.innerHTML = reverseBlockCount;
-		if ( ! blocksIds[properties.last_irreversible_block_num]) {
-			blocksIds[properties.last_irreversible_block_num] = true;
-			let operations = {};
-			let operationsCount = 0;
-			block.transactions.forEach(function(transaction) {
-				transaction.operations.forEach(function(operation) {
-					if ( ! operations[operation[0]]) operations[operation[0]] = 0;
-					operations[operation[0]]++;
-					operationsCount++;
-				});
-			});
-			let operationsStr = '';
-			for (let key in operations) {
-				operationsStr += `<a class="btn btn-outline-info btn-sm">${key} <span class="badge badge-info">${operations[key]}</span></a> `; 
-			}
-			
-			let $newRow = $recentBlocksTableTbody.insertRow(0);
-			$newRow.innerHTML = `<tr>
-									<td><a href="#block/${properties.last_irreversible_block_num}">${properties.last_irreversible_block_num}</a></td>
-									<td>${block.timestamp}</td>
-									<td><a href="#account/${block.witness}">${block.witness}</a></td>
-									<td>${block.transactions.length}</td>
-									<td>${operationsCount}</td>
-								</tr>`;
-			
-			$newRow = $recentBlocksTableTbody.insertRow(1);
-			if (operationsStr) $newRow.innerHTML = `<tr>
-									<td colspan="5">${operationsStr}</td>
-								</tr>`;
-		}
 	});
+});
 
-}, 1000);
+golos.api.streamBlock(function(err, block) {
+	
+	let operations = {};
+	let operationsCount = 0;
+	block.transactions.forEach(function(transaction) {
+		transaction.operations.forEach(function(operation) {
+			if ( ! operations[operation[0]]) operations[operation[0]] = 0;
+			operations[operation[0]]++;
+			operationsCount++;
+		});
+	});
+	let operationsStr = '';
+	for (let key in operations) {
+		operationsStr += `<a class="btn btn-outline-info btn-sm">${key} <span class="badge badge-info">${operations[key]}</span></a> `; 
+	}
+
+	let $newRow = $recentBlocksTableTbody.insertRow(0);
+	$newRow.innerHTML = `<tr>
+							<td><a href="#block/${lastBlock}">${lastBlock}</a></td>
+							<td>${block.timestamp}</td>
+							<td><a href="#account/${block.witness}">${block.witness}</a></td>
+							<td>${block.transactions.length}</td>
+							<td>${operationsCount}</td>
+						</tr>`;
+
+	$newRow = $recentBlocksTableTbody.insertRow(1);
+	if (operationsStr) $newRow.innerHTML = `<tr>
+							<td colspan="5">${operationsStr}</td>
+						</tr>`;
+
+});
 
 let editor;
 
