57af88c7d06077321114494fdde6e0b786714b36
Sing Painter <mail@erlan.xyz>
2018-08-30 00:11:27 +0300

    * fix new bug in #74

--- init.js
+++ init.js
@@ -5,32 +5,6 @@ let $nodeAddressInput = $nodeAddress.querySelector('.form-control[name="node-add
 if (localStorage && localStorage.nodeAddress) $nodeAddressInput.value = localStorage.nodeAddress;
 let nodeAddress = $nodeAddressInput.value;
 
-let getBlockchainVersion = (nodeAddress, callback) => {
-	let socket = new WebSocket(nodeAddress);
-	socket.onopen = (event) => {
-		socket.send(JSON.stringify({
-			jsonrpc: '2.0',
-			id: 1,
-			method: 'call',
-			params: ['database_api', 'get_config', [0], ]
-		}));
-		socket.onmessage = (raw) => {
-			let data = JSON.parse(raw.data);
-			socket.close();
-			callback(null, data.result);
-		};
-	};
-	socket.onerror = (event) => {
-		console.error('onerror', event);
-		callback(event.code, null);
-	}
-};
-
-getBlockchainVersion(nodeAddress, (err, result) => {
-	if (result && parseFloat(result.STEEMIT_BLOCKCHAIN_VERSION) < 0.18) golosJsVersion = '0.6.3';
-	loadGolosJsLib();
-});
-
 let loadGolosJsLib = () => {
 	let golosJsLib = document.createElement('script');
 	golosJsLib.src = `https://cdn.jsdelivr.net/npm/golos-js@${golosJsVersion}/dist/golos.min.js`;
@@ -41,3 +15,36 @@ let loadGolosJsLib = () => {
 	};
 	(document.head || document.documentElement).appendChild(golosJsLib);
 };
+
+let getBlockchainVersion = (nodeAddress, callback) => {
+	let socket;
+	try {
+		socket = new WebSocket(nodeAddress);
+	}
+	catch (e) {}
+	if (socket) {
+		socket.onopen = (event) => {
+			socket.send(JSON.stringify({
+				jsonrpc: '2.0',
+				id: 1,
+				method: 'call',
+				params: ['database_api', 'get_config', [0], ]
+			}));
+			socket.onmessage = (raw) => {
+				let data = JSON.parse(raw.data);
+				socket.close();
+				callback(null, data.result);
+			};
+		};
+		socket.onerror = (event) => {
+			console.error('onerror', event);
+			callback(event.code);
+		}
+	}
+	else callback(true);
+};
+
+getBlockchainVersion(nodeAddress, (err, result) => {
+	if (result && parseFloat(result.STEEMIT_BLOCKCHAIN_VERSION) < 0.18) golosJsVersion = '0.6.3';
+	loadGolosJsLib();
+});
